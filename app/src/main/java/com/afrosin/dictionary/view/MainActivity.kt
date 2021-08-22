cd80078259022e967dd5ca1bc4b6cef8fe18318c
Alexey <a.afrosin@tarifer.ru>
2021-08-22 13:15:31 +0400

    Четверть 4. Урок 6. добавил проверку обновления

--- app/src/main/java/com/afrosin/dictionary/view/MainActivity.kt
+++ app/src/main/java/com/afrosin/dictionary/view/MainActivity.kt
@@ -18,6 +18,14 @@ import com.afrosin.dictionary.viewmodels.convertMeaningsToString
 import com.afrosin.model.data.AppState
 import com.afrosin.model.data.DataModel
 import com.afrosin.utils.network.isOnline
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.play.core.appupdate.AppUpdateManager
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.InstallState
+import com.google.android.play.core.install.InstallStateUpdatedListener
+import com.google.android.play.core.install.model.AppUpdateType.IMMEDIATE
+import com.google.android.play.core.install.model.InstallStatus
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.google.android.play.core.splitinstall.SplitInstallManager
 import com.google.android.play.core.splitinstall.SplitInstallManagerFactory
 import com.google.android.play.core.splitinstall.SplitInstallRequest
@@ -26,6 +34,7 @@ import org.koin.android.viewmodel.ext.android.viewModel
 private const val HISTORY_SEARCH_ACTIVITY_PATH =
     "com.afrosin.historyscreen.view.history.HistorySearchWordActivity"
 private const val HISTORY_SEARCH_ACTIVITY_FEATURE_NAME = "historyScreen"
+private const val UPDATE_REQUEST_CODE = 42
 
 class MainActivity : BaseActivity<AppState, MainInteractor>() {
 
@@ -33,6 +42,8 @@ class MainActivity : BaseActivity<AppState, MainInteractor>() {
     override lateinit var activityViewModel: MainViewModel
 
     private lateinit var splitInstallManager: SplitInstallManager
+
+    private lateinit var appUpdateManager: AppUpdateManager
 
 
     private val adapter: MainAdapter by lazy { MainAdapter(onListItemClickListener) }
@@ -79,7 +90,53 @@ class MainActivity : BaseActivity<AppState, MainInteractor>() {
 
         iniViewModel()
         initViews()
+        checkForUpdates()
 
+    }
+
+    private fun checkForUpdates() {
+        appUpdateManager = AppUpdateManagerFactory.create(applicationContext)
+
+        val appUpdateInfo = appUpdateManager.appUpdateInfo
+
+        appUpdateInfo.addOnSuccessListener { appUpdateIntent ->
+            if (appUpdateIntent.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                && appUpdateIntent.isUpdateTypeAllowed(IMMEDIATE)
+            ) {
+                appUpdateManager.registerListener(stateUpdateListener)
+
+                appUpdateManager.startUpdateFlowForResult(
+                    appUpdateIntent,
+                    IMMEDIATE,
+                    this,
+                    UPDATE_REQUEST_CODE
+                )
+            }
+        }
+
+
+    }
+
+    private val stateUpdateListener: InstallStateUpdatedListener =
+        object : InstallStateUpdatedListener {
+            override fun onStateUpdate(state: InstallState) {
+                state.let {
+                    if (it.installStatus() == InstallStatus.DOWNLOADED) {
+                        popupSnackBarForCompleteUpdate()
+                    }
+                }
+            }
+        }
+
+    private fun popupSnackBarForCompleteUpdate() {
+        Snackbar.make(
+            findViewById(R.id.activity_main_layout),
+            "Обновление готово к установке",
+            Snackbar.LENGTH_INDEFINITE
+        ).apply {
+            setAction("RESTART") { appUpdateManager.completeUpdate() }
+            show()
+        }
     }
 
     private fun initViews() {
@@ -154,4 +211,37 @@ class MainActivity : BaseActivity<AppState, MainInteractor>() {
         vb.mainLoadingLayout.loadingFrameLayout.visibility = View.GONE
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == UPDATE_REQUEST_CODE) {
+            appUpdateManager.unregisterListener(stateUpdateListener)
+        } else {
+            Toast.makeText(
+                applicationContext,
+                "Update flow failed! Result code: $resultCode",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        appUpdateManager
+            .appUpdateInfo
+            .addOnSuccessListener { appUpdateInfo ->
+                if (appUpdateInfo.installStatus() == InstallStatus.DOWNLOADED) {
+                    popupSnackBarForCompleteUpdate()
+                }
+                if (appUpdateInfo.updateAvailability() == UpdateAvailability.DEVELOPER_TRIGGERED_UPDATE_IN_PROGRESS) {
+                    appUpdateManager.startUpdateFlowForResult(
+                        appUpdateInfo,
+                        IMMEDIATE,
+                        this,
+                        UPDATE_REQUEST_CODE
+                    )
+                }
+            }
+    }
 }
